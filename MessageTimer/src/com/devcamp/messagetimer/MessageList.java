0b8e3c6e141c6b23658c2ca109f0ff715b18e8de
pospisd <pospisd@gmail.com>
2012-02-18 16:42:51 +0100

    MessagList added

---
+++ MessageTimer/src/com/devcamp/messagetimer/MessageList.java
@@ -0,0 +1,11 @@
+package com.devcamp.messagetimer;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class MessageList extends ListActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	
+    }
+}
