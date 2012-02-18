cc268a4482bce5ffb541a05b59ab00dc9afa7bb5
Joe Scurab <scurab@centrum.cz>
2012-02-18 15:31:21 +0100

    Add Message entity

---
+++ MessageTimer/src/com/devcamp/messagetimer/model/Message.java
@@ -0,0 +1,12 @@
+package com.devcamp.messagetimer.model;
+
+import java.util.Date;
+
+public class Message
+{
+	public long id;
+	public String phoneNumber;
+	public String message;
+	public Date when;
+	public boolean isEnabled;
+}
