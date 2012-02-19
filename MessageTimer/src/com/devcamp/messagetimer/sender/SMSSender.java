84ad7d8839af3d6b6d83833b17d5ba468b0bfc95
Joe Scurab <scurab@centrum.cz>
2012-02-19 15:59:55 +0100

    Add code for send sms

--- MessageTimer/src/com/devcamp/messagetimer/sender/SMSSender.java
+++ MessageTimer/src/com/devcamp/messagetimer/sender/SMSSender.java
@@ -3,6 +3,7 @@ package com.devcamp.messagetimer.sender;
 import com.devcamp.messagetimer.tools.PropertyProvider;
 
 import android.content.Context;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -18,6 +19,8 @@ public class SMSSender extends Sender
 	public void sendMessage(String to, String message)
 	{
 //		Toast.makeText(mContext.getApplicationContext(), "SMS Send to " + to, Toast.LENGTH_LONG).show();
+		SmsManager sm = SmsManager.getDefault();
+//		sm.sendTextMessage(to, null, message, null, null);
 		Log.v("SMSSender","sendMessage");
 	}
 
