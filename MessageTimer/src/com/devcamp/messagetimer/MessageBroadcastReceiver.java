0cd5086f0c8088723159de002caf3b3d6487dca6
Joe Scurab <scurab@centrum.cz>
2012-02-19 13:28:59 +0100

    Update MessageBroadcastReceiver.java

--- MessageTimer/src/com/devcamp/messagetimer/MessageBroadcastReceiver.java
+++ MessageTimer/src/com/devcamp/messagetimer/MessageBroadcastReceiver.java
@@ -1,5 +1,12 @@
 package com.devcamp.messagetimer;
 
+import com.devcamp.messagetimer.model.Message;
+import com.devcamp.messagetimer.sender.Sender;
+import com.devcamp.messagetimer.tools.Database;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -15,18 +22,73 @@ public class MessageBroadcastReceiver extends BroadcastReceiver
 			@Override
 			public void run()
 			{
-				onReceiveImpl(context,intent);
+				
+				try
+				{
+					onReceiveImpl(context,intent);
+				}
+				catch(Exception e)
+				{
+					//TODO whatTODO
+				}
 			}
 		},"MessageReceiver.onReceive").start();
 	}
 	
-	private void onReceiveImpl(Context context, Intent intent)
+	private void onReceiveImpl(Context context, Intent intent) throws ClassNotFoundException, IllegalAccessException, InstantiationException
 	{
-		
+		Throwable error = null;
+		Message m = null;
+		try
+		{
+			Long id = intent.getLongExtra(MT.MESSAGE_ID, 0);
+			if(id != 0)
+			{
+				Database d = new Database(context);
+				m = d.getMessages(id).get(0);
+				sendMessage(m);
+			}
+		}
+		catch(Throwable t)
+		{
+			error = t;
+		}
+		showNotification(context, m, error);
 	}
 	
-//	private void notifyUserAboutSend()
-//	{
-//		
-//	}
+	private void sendMessage(Message m) throws ClassNotFoundException, IllegalAccessException, InstantiationException
+	{
+		Sender s = getSender(m.service);
+		s.sendMessage(m.phoneNumber, m.message);
+	}
+	
+	private Sender getSender(String service) throws ClassNotFoundException, IllegalAccessException, InstantiationException
+	{
+		Class c = Class.forName(service);
+		Sender s = (Sender) c.newInstance();
+		return s;
+	}
+	
+	/**
+	 * Shows notification
+	 * @param c 
+	 * @param m message which has been sent
+	 * @param t if any error occur, pass error to notify user, can be null
+	 */
+	private void showNotification(Context c, Message m, Throwable t)
+	{
+		NotificationManager nm = (NotificationManager) c.getSystemService(Context.NOTIFICATION_SERVICE);				
+		PendingIntent pi = PendingIntent.getBroadcast(c, 0, new Intent(), 0);
+		Notification n = new Notification(R.drawable.logo, m.contact, System.currentTimeMillis());	
+		n.flags = Notification.FLAG_ONGOING_EVENT;
+		String title = String.format("%s (%s)",m.contact,m.phoneNumber);
+		String summary = null;
+		if(t == null)
+			summary = c.getString(R.string.txtMessageHasBeenSent);
+		else
+			summary = "ERROR:" + t.getMessage();
+		
+		n.setLatestEventInfo(c, title, summary, pi);
+		nm.notify(1, n);
+	}
 }
